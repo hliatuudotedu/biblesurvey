b436c3e72fbab850197015c82cebd17008bbf054
Max Li <max.li@live.com>
2016-08-10 15:00:39 -0500

    Heroku test

--- survey/views.py
+++ survey/views.py
@@ -62,6 +62,7 @@ def survey_processing(request, provider_name, survey_name):
         form = SurveyForm(request.POST)
         choice_ids_list = list()
 
+
         patient_name = request.POST['patient_name']
 
         for key, value in form.data.items():
