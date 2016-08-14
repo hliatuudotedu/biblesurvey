dc4e41dd25f1e5764351b6b328a5cc6b6061eb1a
Max Li <max.li@live.com>
2016-08-13 22:32:32 -0500

    Change admin.py

--- survey/admin.py
+++ survey/admin.py
@@ -1,8 +1,16 @@
 from django.contrib import admin
 
 from .models import (
-    Category, Question, Choice)
+    Provider,
+    Survey,
+    Category,
+    Question,
+    Choice,
+    SurveyQuestion)
 
+admin.site.register(Provider)
+admin.site.register(Survey)
 admin.site.register(Category)
 admin.site.register(Question)
 admin.site.register(Choice)
+admin.site.register(SurveyQuestion)
