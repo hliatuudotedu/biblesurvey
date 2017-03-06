20121fb77c8b7c085a49f799fd07ba62c0a2b15b
lancedelljr <lancedelljr@gmail.com>
2017-03-06 14:45:22 -0600

    edit settings

--- biblesurvey/settings.py
+++ biblesurvey/settings.py
@@ -126,4 +126,3 @@ USE_TZ = True
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (os.path.join(BASE_DIR, '../static'))
